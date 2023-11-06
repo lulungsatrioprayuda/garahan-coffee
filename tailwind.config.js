1d6be647894c5b29c53dc06e5b95d13a07db613b
lulung <alungsatrio12@gmail.com>
2023-11-06 21:53:20 +0700

    setup color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutralSilver: "#F5F7FA",
+        neutralDGrey: "#4D4DAD",
+        brandPrimary: "#4CAF4F",
+        neturalGrey: "#717171",
+      },
+    },
   },
   plugins: [],
 };
