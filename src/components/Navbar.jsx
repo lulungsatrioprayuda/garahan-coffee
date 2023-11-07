19aa90e41faeaff9efc34c4820ac19d27ffbe7b3
lulung <alungsatrio12@gmail.com>
2023-11-07 20:35:05 +0700

    progress navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Navbar = () => {
-  const [] = useState(0);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
+
+  //for toggle menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  //nav items array
+  const navItems = [
+    { link: "Home", path: "home" },
+    { link: "Service", path: "service" },
+    { link: "Product", path: "product" },
+    { link: "Testimonial", path: "testimonial" },
+    { link: "FAQ", path: "faq" },
+  ];
   return (
-    <div>
-      <h1>navbar</h1>
-    </div>
+    <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">
+      <nav>
+        <div>
+          <a href=""></a>
+        </div>
+      </nav>
+    </header>
   );
 };
 
