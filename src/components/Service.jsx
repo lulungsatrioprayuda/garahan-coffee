d7249d5228b7de157421cecadf751c033016de6f
lulung <alungsatrio12@gmail.com>
2023-11-10 00:55:37 +0700

    service section

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 const Service = () => {
   return (
-    <div>
-      <div>Servcice</div>
+    <div className="md:px-14 px-4 py-16 max-w-screen-2xl mx-auto">
+      <div className="text-center my-8">
+        <h2 className="text-4xl text-neutralDGrey font-semibold mb-2">
+          Our Distribution
+        </h2>
+        <p className="text-neutralGrey">
+          Our Coffee Products are Available in Most Culinary Souvenir Shops
+          Across Every District in East Java.
+        </p>
+        {/* company logo */}
+      </div>
     </div>
   );
 };
